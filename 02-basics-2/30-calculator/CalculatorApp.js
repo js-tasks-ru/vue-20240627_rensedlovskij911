ca736719b7de621428463538f6d0510827d56f4c
Renat Sedlovsky <ren.sedlovskij2009@yandex.ru>
2024-08-19 15:50:52 +0500

    Решение задачи calculator

--- 02-basics-2/30-calculator/CalculatorApp.js
+++ 02-basics-2/30-calculator/CalculatorApp.js
@@ -1,26 +1,49 @@
-import { defineComponent } from 'vue'
+import { defineComponent, ref, computed } from 'vue/dist/vue.esm-bundler.js'
 
 export default defineComponent({
   name: 'CalculatorApp',
 
-  setup() {},
+  setup() {
+    const first_value = ref(0)
+    const second_value = ref(0)
+    const operator = ref('sum')
+
+    const answer = computed(() => {
+      if (operator.value === 'sum') {
+        return first_value.value + second_value.value
+      } else if (operator.value === 'subtract') {
+        return first_value.value - second_value.value
+      } else if (operator.value === 'multiply') {
+        return first_value.value * second_value.value
+      } else if (operator.value === 'divide') {
+        return first_value.value / second_value.value
+      }
+    })
+
+    return {
+      first_value,
+      second_value,
+      operator,
+      answer,
+    }
+  },
 
   template: `
     <div class="calculator">
-      <input type="number" aria-label="First operand" />
+      <input type="number" aria-label="First operand" v-model="first_value"/>
 
       <div class="calculator__operators">
-        <label><input type="radio" name="operator" value="sum"/>➕</label>
-        <label><input type="radio" name="operator" value="subtract"/>➖</label>
-        <label><input type="radio" name="operator" value="multiply"/>✖</label>
-        <label><input type="radio" name="operator" value="divide"/>➗</label>
+        <label><input type="radio" name="operator" :checked = "operator === 'sum'" value="sum" v-model="operator" />➕</label>
+        <label><input type="radio" name="operator" value="subtract" v-model="operator"/>➖</label>
+        <label><input type="radio" name="operator" value="multiply" v-model="operator"/>✖</label>
+        <label><input type="radio" name="operator" value="divide" v-model="operator"/>➗</label>
       </div>
 
-      <input type="number" aria-label="Second operand" />
+      <input type="number" aria-label="Second operand" v-model="second_value"/>
 
       <div>=</div>
 
-      <output>0</output>
+      <output>{{ answer }}</output>
     </div>
   `,
 })
